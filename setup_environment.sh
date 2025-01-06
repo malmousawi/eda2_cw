7369f1dc689f49fa1c49f3b58002b56bf797cee9
malmousawi <96029001+malmousawi@users.noreply.github.com>
2025-01-06 01:21:54 +0000

    Create setup_environment.sh

---
+++ setup_environment.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Install git (auto-approve)
+echo "Installing git..."
+sudo dnf install -y git
+
+# Clone the repositories
+echo "Cloning repositories..."
+git clone https://github.com/owainkenwayucl/ds4eng-infra.git
+git clone https://github.com/malmousawi/eda1_cw.git
+
+# Copy files from eda1_cw to ds4eng-infra
+echo "Copying files..."
+sudo cp -r /home/almalinux/eda1_cw/* /home/almalinux/ds4eng-infra/cnc-environment
+
+# Install yum-utils (auto-approve)
+echo "Installing yum-utils..."
+sudo yum install -y yum-utils
+
+# Add HashiCorp repo and install Terraform (auto-approve)
+echo "Adding HashiCorp repo and installing Terraform..."
+sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+sudo yum -y install terraform
+
+# Install EPEL repository and Ansible (auto-approve)
+echo "Installing EPEL repository and Ansible..."
+sudo yum install -y epel-release
+sudo yum install -y ansible
+
+# Change ownership of the ds4eng-infra/cnc-environment directory
+echo "Changing ownership of the ds4eng-infra/cnc-environment directory..."
+sudo chown -R almalinux:almalinux /home/almalinux/ds4eng-infra/cnc-environment
+
+# Move into the cnc-environment directory
+echo "Navigating to the cnc-environment directory..."
+cd /home/almalinux/ds4eng-infra/cnc-environment
+
+echo "Setup complete!"
